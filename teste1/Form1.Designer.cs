4cba032b10d0f7e494c8f634f1bdcc14224d6576
Guilherme WilliamGui <gui-w-lima@hotmail.com>
2020-09-09 10:20:17 -0300

    Adcionado um label na tela do forms

--- teste1/Form1.Designer.cs
+++ teste1/Form1.Designer.cs
@@ -52,6 +52,7 @@
             this.BTN_SALVAR = new System.Windows.Forms.Button();
             this.BTN_INSERIR = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.label12 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -253,11 +254,21 @@
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(501, 264);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(46, 13);
+            this.label12.TabIndex = 24;
+            this.label12.Text = "oi clecio";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(871, 576);
+            this.Controls.Add(this.label12);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.BTN_INSERIR);
             this.Controls.Add(this.BTN_SALVAR);
@@ -315,6 +326,7 @@
         private System.Windows.Forms.Button BTN_SALVAR;
         private System.Windows.Forms.Button BTN_INSERIR;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label12;
     }
 }
 
